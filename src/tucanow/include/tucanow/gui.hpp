0e289b3d6bf236eb62e2179312d8f5d3cfcbd86a
Julio Daniel <jd.silva@ucalgary.ca>
2019-04-03 10:04:21 -0600

    Update tucanow

--- src/tucanow/include/tucanow/gui.hpp
+++ src/tucanow/include/tucanow/gui.hpp
@@ -1,6 +1,9 @@
 #ifndef __TUCANOW_GUI__
 #define __TUCANOW_GUI__
 
+/** @file gui.hpp tucanow/gui.hpp
+ * */
+
 
 #include <memory>
 #include <string>
@@ -22,13 +25,35 @@ class Scene;
 class Gui 
 {
     public: 
+        /**
+         * @brief Constructor implements the pimpl idiom
+         */
         Gui(Scene&);
+
+        /**
+         * @brief Default destructor
+         */
         virtual ~Gui();
 
+
+        /**
+         * @brief Deleted copy constructor
+         */
         Gui(Gui &) = delete;
+
+        /**
+         * @brief Deleted copy assignment
+         */
         Gui& operator=(Gui &) = delete;
 
+        /**
+         * @brief Deleted move constructor
+         */
         Gui(Gui &&) = delete;
+
+        /**
+         * @brief Deleted move assignment
+         */
         Gui& operator=(Gui&&) = delete;
 
         /**
@@ -36,60 +61,65 @@ class Gui
          *
          * Must be called after Glew has been initialized
          *
-         * @param Viewport width 
-         * @param Viewport height 
-         * @param Path to dir where gui's assets can be found
+         * @param width Viewport width 
+         * @param height Viewport height 
+         * @param assets_dir Path to dir where gui's assets can be found
          */
         virtual void initialize(int width, int height, std::string assets_dir = "./samples/assets/");
 
         /**
          * @brief Set gui's scene viewport
          *
-         * @param Viewport width
-         * @param Viewport height
+         * @param width Viewport width
+         * @param height Viewport height
          */
         virtual bool setViewport(int width, int height);
 
         /**
-         * Render gui.
+         * @brief Render gui.
          **/
         virtual void render();
 
         /**
-         * @brief Treats mouse left click callback for all elements
+         * @brief Handles mouse left click callback for all elements
          *
-         * @param Mouse x position
-         * @param Mouse y position
+         * @param xpos Mouse x position
+         * @param ypos Mouse y position
          *
          * @return True if any gui element was clicked, false otherwise
          */
         virtual bool leftButtonPressed(float xpos, float ypos);
 
         /**
-         * @brief Treats mouse left release callback for last clicked element
+         * @brief Handles mouse left release callback for last clicked element
          *
-         * @param Mouse x position
-         * @param Mouse y position
+         * @param xpos Mouse x position
+         * @param ypos Mouse y position
          *
          * @return True if any element is selected, false otherwise
          */
         virtual bool leftButtonReleased(float xpos, float ypos);
 
         /**
-         * @brief Treats mouse mouvement
+         * @brief Handles mouse movement
          *
-         * @param Mouse x position
-         * @param Mouse y position
+         * @param xpos Mouse x position
+         * @param ypos Mouse y position
          *
          * @return True if any element is selected, false otherwise
          */
         virtual bool cursorMove(float xpos, float ypos);
 
     protected:
+        /**
+         * @brief Get a pointer to Tucano's gui base class
+         *
+         * @return Pointer to Tucano::GUI::Base
+         */
         virtual Tucano::GUI::Base* getTucanoGui();
 
-        Scene& scene;
-        std::unique_ptr<GuiImpl> pimpl;
+        Scene& scene; ///<-- Reference to Scene object
+        std::unique_ptr<GuiImpl> pimpl; ///<-- Tucano data
 };
 
 
